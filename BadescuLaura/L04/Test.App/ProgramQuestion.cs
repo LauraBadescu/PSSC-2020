14167df70507381cc0b201351de9f2a46c65148f
Badescu Laura <badescu_laura_01@yahoo.com>
2020-10-28 17:29:34 +0200

    L04

---
+++ BadescuLaura/L04/Test.App/ProgramQuestion.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Test.App
+{
+    class ProgramQuestion
+    {
+        static void Main(string[] args)
+        {
+            var cmd = new CreateQuestionCmd("titlu intrebare", string.Empty, "continutul intrebarii");
+            var result = CreateQuestion(cmd);
+
+            var createQuestionEvent = result.Match(ProcessQuetionPosted, ProcessQuestionNotPosted, ProcessInvalidQuestion);
+
+            Console.ReadLine();
+        }
+
+        private static object CreateQuestion(CreateQuestionCmd cmd)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static ICreateQuestionResult ProcessInvalidQuestion(QuestionValidationFailed validationErrors)
+        {
+            Console.WriteLine("Question validation failed: ");
+            foreach (var error in validationErrors.ValidationErrors)
+            {
+                Console.WriteLine(error);
+            }
+            return (ICreateQuestionResult)validationErrors;
+        }
+
+        private static ICreateQuestionResult ProcessQuestionNotPosted(QuestionNotPosted questionNotPosted)
+        {
+            Console.WriteLine($"Question wasn't posted: {questionNotPosted.Reason}");
+            return (ICreateQuestionResult)questionNotPosted;
+        }
+
+        private static ICreateQuestionResult ProcessQuetionPosted(QuestionPosted new_question)
+        {
+            Console.WriteLine($"Question {new_question.QuestionTitle}");
+            Console.WriteLine($"Description {new_question.Description}");
+            return (ICreateQuestionResult)new_question;
+        }
+
+        public static ICreateQuestionResult CreateNewQuestion(CreateNewQuestionCmd createQuestion)
+        {
+            if (string.IsNullOrWhiteSpace(createQuestion.Description))
+            {
+                var errors = new List<string>() { "Your question wasn't passed " };
+                return new QuestionValidationFailed(errors);
+            }
+
+            if (string.IsNullOrEmpty(createQuestion.Title))
+            {
+                return new QuestionNotPosted("Question couldn't be verified");
+            }
+
+            var questionTitle = Guid.NewGuid();
+            var result = new QuestionPosted(questionTitle, createQuestion.Description);
+
+            return (ICreateQuestionResult)result;
+        }
+
+        internal class Domain
+        {
+            internal class CreateQuestionWorkflow
+            {
+                internal class CreateQuestionResult
+                {
+                }
+            }
+        }
+    }
+}
+
---
+++ BadescuLaura/L04/Test.App/QuestionPosted.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Test.App
+{
+    internal class QuestionPosted
+    {
+        public QuestionPosted(Guid questionTitle, string description)
+        {
+            QuestionTitle = questionTitle;
+            Description = description;
+        }
+
+        public object QuestionTitle { get; internal set; }
+        public object Description { get; internal set; }
+    }
+}
